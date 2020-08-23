37b60def98153bf57f6c0a22d655349395aed480
Daniel Sobisz <dsobisz97@gmail.com>
2020-08-23 12:09:10 +0200

    Updates

---
+++ precache-manifest.0624cc6d05c7fb2dbb7ebd2c7845c37c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdb6c443dc46d478ba22898ea9059870",
+    "url": "/CompareProducts/index.html"
+  },
+  {
+    "revision": "fb1e38b7cabb88636e67",
+    "url": "/CompareProducts/static/css/main.1045d831.chunk.css"
+  },
+  {
+    "revision": "6089349a973e12c146c1",
+    "url": "/CompareProducts/static/js/2.3cc846cf.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/CompareProducts/static/js/2.3cc846cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb1e38b7cabb88636e67",
+    "url": "/CompareProducts/static/js/main.718439ac.chunk.js"
+  },
+  {
+    "revision": "33282f13baeb963a0c7c",
+    "url": "/CompareProducts/static/js/runtime-main.7e62fabc.js"
+  }
+]);
